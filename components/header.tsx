3e26d8dbd48ddc741139fde514b270f0eff114cc
tem <temurbakhriddinov@gmail.com>
2022-04-29 12:28:55 -0500

    link update

--- components/header.tsx
+++ components/header.tsx
@@ -3,11 +3,23 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header className="mx-auto flex max-w-7xl justify-between p-5">
+    <header className="mx-auto flex max-w-7xl justify-between p-5 !font-SuezOne">
       <div className="flex items-center space-x-5">
         <Link href="/">
-          <div className="cursor-pointer !font-SuezOne text-xl text-color-primary">
+          <div className="cursor-pointer text-xl text-color-primary">
             tembee
+          </div>
+        </Link>
+      </div>
+      <div className="inline-flex items-center space-x-5">
+        <Link href="https://www.tiktok.com/@tembee.app">
+          <div className="text-l cursor-pointer rounded-full bg-color-primary py-1 px-3 text-color-secondary">
+            tiktok
+          </div>
+        </Link>
+        <Link href="https://www.instagram.com/tembee.app/">
+          <div className="text-l cursor-pointer rounded-full bg-color-primary py-1 px-3 text-color-secondary">
+            insta
           </div>
         </Link>
       </div>
