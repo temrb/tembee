3a10de024f84ecf9601a6c7920b6e3b544e52106
tem <temurbakhriddinov@gmail.com>
2022-04-30 02:28:38 -0500

    ga test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,28 @@ import { Provider } from 'react-redux'
 import { store } from '../redux/store'
 import Layout from '../components/layout'
 import NextNProgress from 'nextjs-progressbar'
+import * as ga from '../lib/ga'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <Provider store={store}>
       <Layout>
