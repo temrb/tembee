59508ae10f09e7d5794bd0228cc94b6dfd1552eb
tem <temurbakhriddinov@gmail.com>
2022-04-30 03:08:56 -0500

    UI chages

--- components/banner.tsx
+++ components/banner.tsx
@@ -4,10 +4,12 @@ const Banner = () => {
   return (
     <main className="flex justify-center">
       <div className="flex w-fit items-center justify-between bg-color-primary py-10 sm:rounded-lg">
-        <div className="space-y-5 px-10">
-          <h1 className="max-w-xl text-4xl font-semibold text-bg ">
+        <div className="flex items-center space-y-3 gap-6 px-10">
+          <div className="animate-bounce text-4xl">🚀</div>
+          <h1 className="max-w-xl text-center text-4xl font-semibold text-bg">
             discover interesting ideas, tools, and products
           </h1>
+          <div className="animate-bounce text-4xl">🚀</div>
         </div>
       </div>
     </main>
